bf217484da6ea4393583c2bbdeac24c5eefcdd63
Dmitry Artemev <dorvakd9@gmail.com>
2022-02-07 17:14:24 +0300

    Initial Commit

---
+++ photo-mp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  photo-mp
+//
+//  Created by Дмитрий Артемьев on 07.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
