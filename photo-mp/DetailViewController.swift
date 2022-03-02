d17f8909ad5fa0893080c5edec0456fb5185ad18
Dmitry Artemev <dorvakd9@gmail.com>
2022-03-02 20:55:24 +0300

    Добавлен экран детальной страницы

---
+++ photo-mp/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  photo-mp
+//
+//  Created by Дмитрий Артемьев on 02.03.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
